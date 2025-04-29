66f50fb205d0ca1e0fe48fbda817f7e56c61d36e
Your Name <Leduckhanh090304@gmail.com>
2025-04-29 22:10:42 +0700

    Initial commit

--- src/main/java/baitaplon/app.java
+++ src/main/java/baitaplon/app.java
@@ -4,15 +4,15 @@ import org.springframework.boot.SpringApplication;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
- 
+
 
 @SpringBootApplication
 
 public class app {
 
-       public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    	   SpringApplication.run(app.class, args);
+        SpringApplication.run(app.class, args);
 
     }
-  }
+}
